c57d57e1296a84dadaaab213b34671b7bd918e56
InkyDChoyee <choyee0421@naver.com>
2023-12-05 17:45:07 +0900

    feat.mainPic_change, selecet

---
+++ MAIN/main_script.js
@@ -0,0 +1,38 @@
+let mainPic = ["img/main-pic1.jpg", "img/main-pic2.jpg", "img/main-pic3.jpg", "img/main-pic4.jpg", "img/main-pic5.jpg"];
+let picIdx = 0;
+let mainProdPic = document.querySelector(".main-prod-pic");
+let selPic = [".pic1", ".pic2", ".pic3", ".pic4", ".pic5"];
+
+showSlide();
+
+
+function showSlide() {
+    mainProdPic.src = mainPic[picIdx];
+    picIdx++;
+    if(picIdx == mainPic.length){
+        picIdx = 0;
+    }
+    
+    
+    setTimeout(showSlide, 3000);
+}
+
+function nextSlide() {
+    mainProdPic.src = mainPic[picIdx];
+    picIdx++;
+    if(picIdx == mainPic.length){
+        picIdx = 0;
+    }
+}
+
+function beforeSlide() {
+    mainProdPic.src = mainPic[picIdx];
+    picIdx--;
+    if(picIdx == 0){
+        picIdx = mainPic.length;
+    }
+}
+
+function selectPic(i) {
+    mainProdPic.src = mainPic[i];
+}
