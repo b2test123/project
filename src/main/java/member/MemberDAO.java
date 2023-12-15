4173722dfe3a130dde7385f32c2961b963d4ee44
dbstjrals130 <145078936+dbstjrals130@users.noreply.github.com>
2023-12-15 23:01:23 +0900

    Update MemberDAO.java

--- src/main/java/member/MemberDAO.java
+++ src/main/java/member/MemberDAO.java
@@ -9,21 +9,19 @@ import java.util.List;
 
 import common.JDBCUtil;
 
-//회원을 추가, 검색, 수정, 삭제할 클래스
 public class MemberDAO {
-	
-	Connection conn = null;         //db 연결 및 종료
-	PreparedStatement pstmt = null; //sql 처리
-	ResultSet rs = null;            //검색한 데이터셋
-	
+	Connection conn = null;         //db연결 및 종료
+	PreparedStatement pstmt = null; //sql처리
+	ResultSet rs = null;			//검색한 데이터 셋
+
 	//회원 목록
 	public List<MemberVO> getMemberList(){
-		List<MemberVO> memberList = new ArrayList<>();
+		List<MemberVO> memberList = new ArrayList();
 		try {
-			//db 연결
+			//db연결
 			conn = JDBCUtil.getConnection();
-			//sql 처리
-			String sql = "SELECT * FROM MemberVO ORDER BY id";
+			//sql처리
+			String sql = "select * from member";
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 			while(rs.next()) {
@@ -34,29 +32,31 @@ public class MemberDAO {
 				m.setName(rs.getString("name"));
 				m.setEmail(rs.getString("email"));
 				m.setPhone(rs.getString("phone"));
-				m.setJoindate(rs.getTimestamp("joindate"));
 				m.setZip_code(rs.getString("zip_code"));
 				m.setAddress(rs.getString("address"));
 				m.setDetailaddress(rs.getString("detailaddress"));
+
 				//리스트에 회원을 추가
 				memberList.add(m);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally { //db 종료
+		} finally { //db종료
 			JDBCUtil.close(conn, pstmt, rs);
 		}
+
+		//db종료
 		return memberList;
-	}
-	
+	} //회원목록
+
 	//회원 가입
 	public void insertMember(MemberVO m) {
+
 		try {
 			conn = JDBCUtil.getConnection();
-			
-			String sql = "INSERT INTO MemberVO (id, passwd, name, email, phone, "
-					+ "zip_code, address, detailaddress) "
-					+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+			String sql = "insert into member(id, passwd, name, email, phone, zip_code, address, detailaddress) "
+					+ "values(?, ?, ?, ?, ?, ?, ?, ?)";
+
 			pstmt = conn.prepareStatement(sql);
 			//폼에 입력된 데이터를 가져와서 db에 저장함
 			pstmt.setString(1, m.getId());
@@ -67,6 +67,7 @@ public class MemberDAO {
 			pstmt.setString(6, m.getZip_code());
 			pstmt.setString(7, m.getAddress());
 			pstmt.setString(8, m.getDetailaddress());
+
 			//sql 실행
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
@@ -74,17 +75,40 @@ public class MemberDAO {
 		} finally {
 			JDBCUtil.close(conn, pstmt);
 		}
+	}//회원가입
+
+	//로그인 인증
+	public MemberVO checkLogin(MemberVO m) {
+		try {
+			conn = JDBCUtil.getConnection();
+			String sql = "select * from member where id = ? and passwd = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, m.getId());
+			pstmt.setString(2, m.getPasswd());
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				m.setName(rs.getString("name"));
+				System.out.println("Login successful. Name: " + m.getName());
+			}else {
+				System.out.println("Login failed.");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(conn, pstmt, rs);
+		}
+		return m;
 	}
-	
-	//회원 정보(상세보기)
+
+	//회원상세조회
 	public MemberVO getMember(String id) {
 		MemberVO m = new MemberVO();
 		try {
 			conn = JDBCUtil.getConnection();
-		
-			String sql = "SELECT * FROM MemberVO WHERE id = ?";
+			String sql = "SELECT * FROM member WHERE id = ?";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
+			
 			rs = pstmt.executeQuery();
 			if(rs.next()) {
 				m.setId(rs.getString("id"));
@@ -92,7 +116,7 @@ public class MemberDAO {
 				m.setName(rs.getString("name"));
 				m.setEmail(rs.getString("email"));
 				m.setPhone(rs.getString("phone"));
-				m.setJoindate(rs.getTimestamp("joindate"));
+				m.setJoinDate(rs.getTimestamp("joindate"));
 				m.setZip_code(rs.getString("zip_code"));
 				m.setAddress(rs.getString("address"));
 				m.setDetailaddress(rs.getString("detailaddress"));
@@ -104,48 +128,4 @@ public class MemberDAO {
 		}
 		return m;
 	}
-	
-	//로그인 인증(객체:member 로 반환)
-	public MemberVO checkLogin(MemberVO m) {
-		try {
-			conn = JDBCUtil.getConnection();
-			String sql = "SELECT * FROM memberVO "
-					+ "WHERE id = ? and passwd = ?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, m.getId());
-			pstmt.setString(2, m.getPasswd());
-			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				//이름을 db에서 가져옴
-				m.setName(rs.getString("name"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			JDBCUtil.close(conn, pstmt, rs);
-		}
-		return m;
-	}
-	
-	
-	//ID 중복 검사
-	public boolean getDuplicatedId(String id) {
-		boolean result = false;
-		
-		try {
-			conn = JDBCUtil.getConnection();
-			String sql = "SELECT DECODE(COUNT(*), 1, 'true', 'false') AS result "
-					+ "FROM member WHERE id = ?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, id);
-			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				//별칭을 칼럼으로 db에서 데이터를 가져옴
-				result = rs.getBoolean("result");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-}//dao 클래스 닫기
+}
